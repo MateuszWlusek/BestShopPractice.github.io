a8d36e192b8ce992e186c49548d7fb446146c3c2
Mateusz Włusek <mateusz.wlusek@gmail.com>
2020-05-14 15:51:15 +0200

    Added animated hamburger and sliding menu

--- js/app.js
+++ js/app.js
@@ -1,33 +1,37 @@
-const button = document.querySelector('.hamburger i');
+const button = document.querySelector('.hamburger');
 const list = document.querySelector('.menu');
 const mobile = window.matchMedia("screen and (max-width:768px");
 console.log(button);
 console.log(list);
 
 if (mobile.matches) {
-    list.style.display = 'none';
+    // list.style.display = 'none';
     button.style.display = 'block';
 } else {
-    list.style.display = 'block';
+    // list.style.display = 'block';
     button.style.display = 'none';
 }
 
 mobile.addListener(function (mobile) {
     if (mobile.matches) {
-        list.style.display = 'none';
+        // list.style.display = 'none';
         button.style.display = 'block';
     } else {
-        list.style.display = 'block';
+        // list.style.display = 'block';
         button.style.display = 'none';
     }
 });
 
+// button.addEventListener('click', function () {
+//     if (list.style.display == 'block') {
+//         list.style.display = 'none';
+//         button.classList.remove('active');
+//     } else {
+//         list.style.display = 'block';
+//         button.classList.add('active');
+//     }
+// });
 button.addEventListener('click', function () {
-    if (list.style.display == 'block') {
-        list.style.display = 'none';
-        button.className = 'fas fa-bars';
-    } else {
-        list.style.display = 'block';
-        button.className = 'fas fa-times';
-    }
+    button.classList.toggle('active');
+    list.classList.toggle('show');
 });
